7ac866a968befe4c235a85969d6eb4780a63eac0
mamokebe <kebedebekele7@gmail.com>
2024-12-29 21:21:10 -0600

    url updated

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   //local baseURL
-  baseURL: "http://localhost:3003/api",
+  // baseURL: "http://localhost:3003/api",
   //deployed base url
   //mine
-  // baseURL: "https://evangadi-forum-backend-mnq9.onrender.com/api",
+  baseURL: "https://evangadi-forum-backend-mnq9.onrender.com/api",
   //group-2
   // baseURL: "https://evangadi-backend-fexo.onrender.com/api",
 });
